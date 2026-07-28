31594cbaacc19c0d2367eea9aa56e650fb310875
Emil Skovbo Uldahl Pedersen <uldahlemil@gmail.com>
2026-07-28 11:30:39 +0200

    Added function to create a table with descriptive statistics

--- R/functions.R
+++ R/functions.R
@@ -7,4 +7,21 @@ preprocess <- function(data) {
       cols = -c(id, lipidosis)) |>
     dplyr::rename("metabolite" = name)
 }
-preprocess(lipoproteins)
+
+create_table_descriptive_stats <- function(data) {
+  data |>
+    preprocess() |>
+    dplyr::summarise(
+      dplyr::across(
+        -c(id, lipidosis),
+        list(mean = mean, sd = sd, median = median,
+             iqr = \(x) IQR(x, na.rm = TRUE))
+      ),
+      .by = c(metabolite)
+    ) |>
+    dplyr::filter(!is.na(c(value_sd))) |>
+    dplyr::mutate(across(where(is.numeric), \(x) round(x, digits = 1))) |>
+    dplyr::mutate(MeanSD = glue::glue("{value_mean} ({value_sd})")) |>
+    dplyr::mutate(MedianIQR = glue::glue("{value_median} ({value_iqr})")) |>
+    dplyr::select(Metabolite = metabolite, "Mean (SD)" = MeanSD, "Median (IQR)" = MedianIQR)
+}
