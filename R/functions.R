7243097ee814e21a7f5c6899fcc1f246c9ac2ebd
Emil Skovbo Uldahl Pedersen <uldahlemil@gmail.com>
2026-07-28 12:07:23 +0200

    Made function to create a distributions plot.

--- R/functions.R
+++ R/functions.R
@@ -38,3 +38,25 @@ create_table_descriptive_stats <- function(data) {
     dplyr::mutate(MedianIQR = glue::glue("{value_median} ({value_iqr})")) |>
     dplyr::select(Metabolite = metabolite, "Mean (SD)" = MeanSD, "Median (IQR)" = MedianIQR)
 }
+
+#' Create a plot showing metabolite distributions
+#'
+#' @param data Lipoproteins data set.
+#'
+#' @returns Plot showing metabolite distributions.
+create_plot_distributions <- function(data) {
+  data |>
+    preprocess() |>
+    dplyr::filter(metabolite %in% c("insulin", "hdl", "ldl", "chol", "na", "k", "cl", "ca", "phosphate")) |>
+    ggplot2::ggplot(aes(x = value)) +
+    ggplot2::geom_histogram() +
+    ggplot2::facet_wrap(vars(metabolite), scales = "free") +
+    ggplot2::theme(
+      plot.title = element_text(hjust = 0.5)
+    ) +
+    ggplot2::labs(
+      x = "Metabolite value",
+      y = "Count",
+      title = "Metabolite value distributions"
+    )
+}
